2d79f3a4c2bf78c7badb19d2a0b9f5458d39ebc2
tungpt <lmht2584399@gmail.com>
2025-05-30 17:09:10 +0700

    x

--- nest-service/src/app.module.ts
+++ nest-service/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.25.231.139', // địa chỉ DB
+      port: 5432, // cổng mặc định PostgreSQL
+      username: 'kequeen', // user DB
+      password: '123456', // pass DB
+      database: 'mydatabase', // tên database
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // nơi chứa entity
+      synchronize: true, // tự động sync entity với DB (chỉ dùng dev)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
